2a9d21e45f04b483255b9ca713d36fc0e4bb0d9c
Ken Anderson <ken.anderson@colorado.edu>
2015-01-14 22:39:50 -0700

    Refactored the logic in before(:each) into a method so it could be called from before(:each) and after(:all)
    Added a new test for updating a contact.

--- test_service.rb
+++ test_service.rb
@@ -15,10 +15,18 @@ describe 'The Contacts Web Service' do
     Sinatra::Application
   end
 
-  before(:each) do
+  def reset_app
     File.delete('db/test.json')
     FileUtils.cp('db/pristine.json', 'db/test.json')
     get '/api/1.0/reset'
+  end
+
+  before(:each) do
+    reset_app
+  end
+
+  after(:all) do
+    reset_app
   end
 
   it "should list all contacts" do
@@ -115,6 +123,38 @@ describe 'The Contacts Web Service' do
     expect(result["email"]).to eq("roy.g.biv@biv.com")
     expect(result["phone"]).to eq("+1 303-555-5500")
     expect(result["twitter"]).to eq("@rainbow")
+  end
+
+  it "should update a contact" do
+    put '/api/1.0/contacts/0', {
+      :expected => {
+        :name      => "Roy G. Biv",
+        :birthdate => "01/01/1901",
+        :email     => "roy.g.biv@biv.com",
+        :phone     => "+1 303-555-5500",
+        :twitter   => "@rainbow"
+      },
+      :updated => {
+        :name      => "Roy Green Biv",
+        :birthdate => "01/01/2001",
+        :email     => "roy@gbiv.com",
+        :phone     => "+1 303-555-5555",
+        :twitter   => "@roygbiv"}
+      }.to_json
+    expect(last_response).to be_ok
+
+    get '/api/1.0/contacts/0'
+    expect(last_response).to be_ok
+    result = JSON.parse(last_response.body)
+    expect(result).to be_an_instance_of(Hash)
+    expect(result.size).to eq(6)
+    expect(result["id"]).to eq(0)
+    expect(result["name"]).to eq("Roy Green Biv")
+    expect(result["birthdate"]).to eq("01/01/2001")
+    expect(result["email"]).to eq("roy@gbiv.com")
+    expect(result["phone"]).to eq("+1 303-555-5555")
+    expect(result["twitter"]).to eq("@roygbiv")
+
   end
 
 end
