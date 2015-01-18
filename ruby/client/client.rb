ac742b0bcd65e668b6da75afad114dfd59f6b7f9
Ken Anderson <ken.anderson@colorado.edu>
2015-01-18 11:03:31 -0700

    Starting to work on the client API and its tests.

--- ruby/client/client.rb
+++ ruby/client/client.rb
@@ -1,19 +1,30 @@
 require 'typhoeus'
 require 'json'
 
-require_relative 'contact'
+require_relative '../model/contact'
 
-def base_uri
-  "http://localhost:3000"
+class NotConnected < StandardError
 end
 
-def available_contacts
-  url      = "#{base_uri}/api/1.0/contacts"
-  response = Typhoeus.get(url)
-  if response.code == 200
-    result = JSON.parse(response.body)
-    result.map {|entry| entry["name"] }
-  else
-    raise response.body
+class ServiceError < StandardError
+end
+
+class Contacts
+
+  def base_uri
+    "http://localhost:3000"
   end
+
+  def available_contacts
+    url      = "#{base_uri}/api/1.0/contacts"
+    response = Typhoeus.get(url)
+    raise NotConnected if response.code == 0
+    if response.code == 200
+      result = JSON.parse(response.body)
+      raise ServiceError if !result["status"]
+      return result["data"]
+    end
+    raise ServiceError
+  end
+
 end
--- ruby/client/test_client.rb
+++ ruby/client/test_client.rb
@@ -1,3 +1,15 @@
 require_relative 'client'
 
-puts available_contacts.inspect
+describe 'The Contacts Client API' do
+
+  before(:each) do
+    @contacts = Contacts.new
+  end
+
+  it "should list available contacts" do
+    list = @contacts.available_contacts
+    expect(list).to be_an_instance_of(Array)
+    expect(list[0]["name"]).to eq("Roy G. Biv")
+  end
+
+end
