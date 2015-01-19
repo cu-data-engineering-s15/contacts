6a5aae9a6746f68d970efad8a98447b0502f38f9
Ken Anderson <ken.anderson@colorado.edu>
2015-01-18 18:55:24 -0700

    Added a new test to create a contact.
    Added some utility functions to ensure that the client API can
    create and accept instances of Contact itself.

--- ruby/client/client.rb
+++ ruby/client/client.rb
@@ -1,6 +1,8 @@
 require 'typhoeus'
 require 'json'
 
+require '../model/contact'
+
 require_relative '../model/contact'
 
 class NotConnected < StandardError
@@ -10,6 +12,13 @@ class ServiceError < StandardError
 end
 
 class Contacts
+
+  def create_contact_from_data(from_response)
+    data = {}
+    from_response.keys.each {|key| data[key.to_sym] = from_response[key] }
+    data[:birthdate] = Date.strptime(data[:birthdate], '%m/%d/%Y')
+    Contact.new(data)
+  end
 
   def base_uri
     "http://localhost:3000"
@@ -25,6 +34,28 @@ class Contacts
       return result["data"]
     end
     raise ServiceError
+  end
+
+  def create(data)
+    url      = "#{base_uri}/api/1.0/contacts"
+    response = Typhoeus.post(url, body: data.to_json)
+    raise NotConnected if response.code == 0
+    if response.code == 200
+      result = JSON.parse(response.body)
+      raise ServiceError if !result["status"]
+      return create_contact_from_data(result['data'])
+    end
+    raise ServiceError
+  end
+
+  def reset
+    url      = "#{base_uri}/api/1.0/reset"
+    response = Typhoeus.get(url)
+    raise NotConnected if response.code == 0
+    if response.code == 200
+      result = JSON.parse(response.body)
+      raise ServiceError if !result["status"]
+    end
   end
 
 end
--- ruby/client/test_client.rb
+++ ruby/client/test_client.rb
@@ -1,15 +1,61 @@
 require_relative 'client'
 
+require_relative '../model/contact'
+
 describe 'The Contacts Client API' do
 
   before(:each) do
     @contacts = Contacts.new
   end
 
+  after(:each) do
+    @contacts = Contacts.new
+    @contacts.reset
+  end
+
   it "should list available contacts" do
     list = @contacts.available_contacts
     expect(list).to be_an_instance_of(Array)
     expect(list[0]["name"]).to eq("Roy G. Biv")
+  end
+
+  it "should create a new contact" do
+    list = @contacts.available_contacts
+    expect(list.size).to eq(2)
+
+    data = {
+      "name"      => "Ken Anderson",
+      "birthdate" => "06/10/1905",
+      "email"     => "ken.anderson@colorado.edu",
+      "phone"     => "+1 303-492-6003",
+      "twitter"   => "@kenbod"
+    }
+
+    c = @contacts.create(data)
+
+    expect(c).to be_an_instance_of(Contact)
+    expect(c.name).to eq("Ken Anderson")
+
+    list = @contacts.available_contacts
+    expect(list.size).to eq(3)
+  end
+
+  it "should delete a contact" do
+  end
+
+  it "should return nil for non-existent contacts" do
+  end
+
+  it "should get a contact" do
+  end
+
+  it "should update a contact" do
+  end
+
+  it "should find a contact" do
+  end
+
+  it "should list upcoming birthdays" do
   end
 
 end
