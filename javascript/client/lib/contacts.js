c4b0fc5da939d7fa73f13ae0a0673ea9c9daf901
Ken Anderson <ken.anderson@colorado.edu>
2015-01-24 09:52:00 -0700

    Added support for getting, deleting, and updating contacts.
    Switched restler code to a single handle_request method and
    changed existing/new methods to make use of it.

--- javascript/client/lib/contacts.js
+++ javascript/client/lib/contacts.js
@@ -25,41 +25,56 @@ var handle_error = function(info, response, callback) {
   }
 }
 
-var handle_get_request = function(url, callback, error) {
-  rest.get(base+url).on('success', function(data) {
-    // console.log("In handle_get_request.success: " + url);
+var handle_request = function(method, url, input, callback, error) {
+  options = {
+    method: method,
+    data: JSON.stringify(input),
+    parser: rest.parsers.json
+  };
+  rest.request(base+url, options).on('success', function(data) {
+    // console.log("In handle_request.success: " + url);
     handle_success(data, callback, error);
   }).on('fail', function(data) {
-    // console.log("In handle_get_request.fail: " + url);
+    // console.log("In handle_request.fail: " + url);
     error(data);
   }).on('error', function(info, response) {
-    // console.log("In handle_get_request.error: " + url);
-    handle_error(info, response, error);
-  });
-}
-
-var handle_post_request = function(url, data, callback, error) {
-  rest.postJson(base+url, data).on('success', function(data) {
-    handle_success(data, callback, error);
-  }).on('fail', function(data) {
-    error(data);
-  }).on('error', function(info, response) {
+    // console.log("In handle_request.error: " + url);
     handle_error(info, response, error);
   });
 }
 
 var reset = function(callback, error) {
-  handle_get_request('/api/1.0/reset', callback, error);
+  handle_request('get', '/api/1.0/reset', [], callback, error);
 }
 
 var available_contacts = function(callback, error) {
-  handle_get_request('/api/1.0/contacts', callback, error);
+  handle_request('get', '/api/1.0/contacts', [], callback, error);
 }
 
 var create_contact = function(data, callback, error) {
-  handle_post_request('/api/1.0/contacts', data, callback, error);
+  handle_request('post', '/api/1.0/contacts', data, callback, error);
+  // handle_post_request('/api/1.0/contacts', data, callback, error);
 }
 
-exports.reset              = reset
-exports.available_contacts = available_contacts
-exports.create_contact     = create_contact
+var delete_contact = function(id, callback, error) {
+  handle_request('delete', '/api/1.0/contacts/' + id, [], callback, error);
+}
+
+var get_contact = function(id, callback, error) {
+  handle_request('get', '/api/1.0/contacts/' + id, [], callback, error);
+}
+
+var update_contact = function(id, existing, updated, callback, error) {
+  var info = {
+    expected: existing,
+    updated: updated
+  };
+  handle_request('put', '/api/1.0/contacts/' + id, info, callback, error);
+}
+
+exports.reset              = reset;
+exports.available_contacts = available_contacts;
+exports.create_contact     = create_contact;
+exports.delete_contact     = delete_contact;
+exports.get_contact        = get_contact;
+exports.update_contact     = update_contact;
